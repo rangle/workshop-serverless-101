cde0930a014aa8a1eef2d1a905fbd73510ed7e3f
meng-fu <meng.fu@rangle.io>
2018-11-19 21:43:35 -0500

    Move click from div to td

--- src/components/ScoreItem.js
+++ src/components/ScoreItem.js
@@ -4,10 +4,10 @@ import faker from "faker";
 const ScoreItem = ({ onUserSelect, user, date, score }) => {
   return (
     <tr>
-      <td>
+      <td onClick={() => onUserSelect(user)} className="user-item">
         <h4 className="ui image header">
           <img alt="" src={faker.image.avatar()} className="ui rounded image" />
-          <div onClick={() => onUserSelect(user)} className="user-item content">
+          <div className="content">
             {user}
             <div className="sub header">{date}</div>
           </div>
