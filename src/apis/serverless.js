aef2eba53c74dcbd7dd013a63f71306f44d846f8
meng-fu <meng.fu@rangle.io>
2018-11-26 16:05:30 -0500

    Restrict api access with api token

--- src/apis/serverless.js
+++ src/apis/serverless.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://rs2n110fhj.execute-api.us-east-2.amazonaws.com/dev/trex-scoreboard"
+  baseURL: "https://rs2n110fhj.execute-api.us-east-2.amazonaws.com/dev/trex-scoreboard",
+  headers: {'x-api-key': 'aZ0qRaDvO71roLf7Xz5iO2n6Li0AEP4e1T9ZXs9i'}
 });
