376b3a9bd541e4986e3511fdfa9b4c12b4d4cdf1
meng-fu <meng.fu@rangle.io>
2018-11-28 10:26:45 -0500

    Fetch api key from env

--- src/apis/serverless.js
+++ src/apis/serverless.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://rs2n110fhj.execute-api.us-east-2.amazonaws.com/dev/trex-scoreboard",
-  headers: {'x-api-key': 'aZ0qRaDvO71roLf7Xz5iO2n6Li0AEP4e1T9ZXs9i'}
+  baseURL:
+    "https://rs2n110fhj.execute-api.us-east-2.amazonaws.com/dev/trex-scoreboard",
+  headers: { "x-api-key": process.env.REACT_APP_API_KEY }
 });
